de6c9a3e514f77108089ef0cf905397aff434dc7
charlotteprobstel <cprobstel@gmail.com>
2023-11-07 20:50:07 +0000

    Added extra code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         makeGetRequest();
+        makePostRequest();
     }
 
     private static void makeGetRequest(){
         try{
-            URL myURL = new URL("https://sefb-lecture5-http-b68d7d1a9cb4.herokuapp.com/");
+            URL myURL = new URL("https://git.heroku.com/sefb-lecture5-http.git/");
             HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "text/html");
@@ -29,5 +33,36 @@ public class Main {
             System.out.println(e.getMessage());
 
         }
+    }
+
+    private static void makePostRequest(){
+        // Set up Body Data
+        String message = "Hello Servlet";
+        byte[] body = message.getBytes(StandardCharsets.UTF_8);
+
+        URL myURL = null;
+        try {
+            myURL = new URL("https://git.heroku.com/sefb-lecture5-http.git/");
+            HttpURLConnection conn = null;
+            conn = (HttpURLConnection) myURL.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Accept","text/html");
+            conn.setRequestProperty("charset","utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(body.length));
+            conn.setDoOutput(true);
+            try(OutputStream outputStream = conn.getOutputStream()){
+                outputStream.write(body,0,body.length);
+            }
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+            String inputLine;
+            while((inputLine = bufferedReader.readLine()) != null){
+                System.out.println(inputLine);
+            }
+            bufferedReader.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
